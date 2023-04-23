563833e61fe8d80b657ca92d15042adeb78dbb4f
Chris Russell <cgrussell@gmail.com>
2023-04-23 12:40:27 +0100

    Cognitive Complexity: Methods and functions for C# and VB

---
+++ src/CodeAnalysis.Domain.Analyzers.Complexity.Tests/CognitiveComplexity/ClassData/MethodCSharp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using CodeAnalysis.Domain.Analyzers.Complexity.Tests.Utilities;
+
+namespace CodeAnalysis.Domain.Analyzers.Complexity.Tests.CognitiveComplexity.ClassData
+{
+    public class MethodCSharp : IEnumerable<object[]>
+    {
+        private const string FileName = "MethodClass.CSharp";
+        private const Language Language = Utilities.Language.CSharp;
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { FileName, "SimpleMethod", 0, Language };
+            yield return new object[] { FileName, "MethodWithInvocation", 0, Language };
+            yield return new object[] { FileName, "MethodWithRecursiveInvocation", 1, Language };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
