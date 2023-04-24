0e866ae5027407ef73e2b29465213401f59948ed
Chris Russell <cgrussell@gmail.com>
2023-04-24 22:29:50 +0100

    Didn't add CSV Utils

--- src/CodeAnalysis.Domain/Extensions/CsvUtilities.cs
+++ src/CodeAnalysis.Domain/Extensions/CsvUtilities.cs
@@ -1,7 +1,15 @@
-﻿namespace CodeAnalysis.Domain.Extensions
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace CodeAnalysis.Domain.Extensions
 {
-    public class CsvUtilities
+    public static class CsvUtilities
     {
-        
+        public static async Task CsvWriteAsync<T>(string path, IEnumerable<T> records)
+        {
+            await using var writer = new StreamWriter(path);
+            await using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+            await csv.WriteRecordsAsync(records);
+        } 
     }
 }
