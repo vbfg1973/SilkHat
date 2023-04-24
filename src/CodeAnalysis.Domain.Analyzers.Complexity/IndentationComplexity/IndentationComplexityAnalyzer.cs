df43944336a208907d74e99100d3ad2c5e6f69b9
Chris Russell <cgrussell@gmail.com>
2023-04-24 22:38:20 +0100

    IndentationComplexityAnalyzer was using locally defined whitespace counter. Now using extension methods that are tested

--- src/CodeAnalysis.Domain.Analyzers.Complexity/IndentationComplexity/IndentationComplexityAnalyzer.cs
+++ src/CodeAnalysis.Domain.Analyzers.Complexity/IndentationComplexity/IndentationComplexityAnalyzer.cs
@@ -1,42 +1,21 @@
-﻿using System.Text.RegularExpressions;
-using CodeAnalysis.Domain.Analyzers.Complexity.Abstract;
+﻿using CodeAnalysis.Domain.Analyzers.Complexity.Abstract;
 
 namespace CodeAnalysis.Domain.Analyzers.Complexity.IndentationComplexity
 {
     public class IndentationComplexityAnalyzer : IIndentationComplexityAnalyzer
     {
-        private readonly string[] _linesArray;
-        public int ComplexityScore { get; private set; }
-
         public IndentationComplexityAnalyzer(IEnumerable<string> lines)
         {
-            _linesArray = lines.CleanLinesArray();
-            ComplexityScore = _linesArray.Sum(CountLeadingWhitespaceIteration);
+            var linesArray = lines.CleanLinesArray();
+            ComplexityScore = linesArray.Sum(str => str.LeadingWhitespaceCount());
         }
-        
+
         public IndentationComplexityAnalyzer(string text)
         {
-            _linesArray = text.Split("\n").CleanLinesArray();
-            ComplexityScore = _linesArray.Sum(CountLeadingWhitespaceIteration);
+            var linesArray = text.Split("\n").CleanLinesArray();
+            ComplexityScore = linesArray.Sum(str => str.LeadingWhitespaceCount());
         }
 
-        private static int CountLeadingWhitespaceIteration(string str)
-        {
-            // Equivalent to: return str.TakeWhile(char.IsWhiteSpace).Count();
-            var length = str.Length;
-            var count = 0;
-            
-            for (var i = 0; i < length; i++)
-            {
-                if (!char.IsWhiteSpace(str[i]))
-                {
-                    break;
-                }
-
-                count++;
-            }
-
-            return count;
-        }
+        public int ComplexityScore { get; }
     }
 }
