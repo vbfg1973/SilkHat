5d4f65e8ef93465a855053f98cc1bd0c7a0b8bcb
Chris Russell <cgrussell@gmail.com>
2023-05-02 01:14:41 +0100

    Fixed line endings normalisation and tests

--- src/SilkHat.Infrastructure.Git.Tests/CommandTests/GitCommitDetailsTests/Data/Messages/RoslynAnalysersGitCommitMessages.cs
+++ src/SilkHat.Infrastructure.Git.Tests/CommandTests/GitCommitDetailsTests/Data/Messages/RoslynAnalysersGitCommitMessages.cs
@@ -8,12 +8,12 @@ namespace SilkHat.Infrastructure.Git.Tests.CommandTests.GitCommitDetailsTests.Da
 
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[] { FileName, "178ece37489aa4ab4426000aba6c62ad0824e2c0", 150 };
-            yield return new object[] { FileName, "38fa7f1501568eff9c5e52d9b572ca5d7666cd65", 29 };
-            yield return new object[] { FileName, "87457b28b9dfce9deb1d804b5bf831d638ad9d3f", 49 };
-            yield return new object[] { FileName, "991cee4ba6efe0380543e00d1bb8151903c98b29", 53 };
-            yield return new object[] { FileName, "bac0aeef523770c8ca917c8b12cb7937f9f871c4", 14 };
-            yield return new object[] { FileName, "ce66272b7ad48534441fe70c7fc0c710ba8a9927", 105 };
+            yield return new object[] { FileName, "178ece37489aa4ab4426000aba6c62ad0824e2c0", 147 };
+            yield return new object[] { FileName, "38fa7f1501568eff9c5e52d9b572ca5d7666cd65", 28 };
+            yield return new object[] { FileName, "87457b28b9dfce9deb1d804b5bf831d638ad9d3f", 48 };
+            yield return new object[] { FileName, "991cee4ba6efe0380543e00d1bb8151903c98b29", 52 };
+            yield return new object[] { FileName, "bac0aeef523770c8ca917c8b12cb7937f9f871c4", 13 };
+            yield return new object[] { FileName, "ce66272b7ad48534441fe70c7fc0c710ba8a9927", 104 };
         }
 
         IEnumerator IEnumerable.GetEnumerator()
