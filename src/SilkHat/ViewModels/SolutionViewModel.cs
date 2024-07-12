d1ea803e9436456764607b718a2559a482693181
Chris Russell <cgrussell@gmail.com>
2024-07-12 19:09:21 +0100

    Tree view displaying but no vertical scrolling possible

--- src/SilkHat/ViewModels/SolutionViewModel.cs
+++ src/SilkHat/ViewModels/SolutionViewModel.cs
@@ -1,6 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using SilkHat.Domain.CodeAnalysis.DotnetProjects.Solutions;
 using SilkHat.Domain.CodeAnalysis.DotnetProjects.Solutions.SolutionAnalysers.Models;
+using SilkHat.Domain.CodeAnalysis.DotnetProjects.Solutions.SolutionAnalysers.ProjectStructure;
 
 namespace SilkHat.ViewModels
 {
@@ -14,6 +18,42 @@ namespace SilkHat.ViewModels
         {
             _solutionCollection = solutionCollection;
             SolutionModel = solutionModel;
+
+            MapSolutionToTreeStructure().Wait();
         }
+
+        public ObservableCollection<SolutionTreeNodeViewModel> Nodes { get; } = new();
+
+
+        #region Map Solution To Tree Structure
+
+        private async Task MapSolutionToTreeStructure()
+        {
+            foreach (ProjectModel project in (await _solutionCollection.ProjectsInSolution(SolutionModel)).OrderBy(x =>
+                         x.Name))
+            {
+                ProjectStructureModel projectStructure = await _solutionCollection.ProjectStructure(project);
+
+                if (TryMapStructureToTreeNode(projectStructure,
+                        out SolutionTreeNodeViewModel solutionTreeNodeViewModel))
+                    Nodes.Add(solutionTreeNodeViewModel);
+            }
+        }
+
+        private bool TryMapStructureToTreeNode(ProjectStructureModel projectStructureModel,
+            out SolutionTreeNodeViewModel solutionTreeNodeViewModel)
+        {
+            solutionTreeNodeViewModel = new SolutionTreeNodeViewModel(projectStructureModel);
+
+            foreach (ProjectStructureModel child in projectStructureModel.Children)
+            {
+                if (TryMapStructureToTreeNode(child, out SolutionTreeNodeViewModel childNode))
+                    solutionTreeNodeViewModel.Children.Add(childNode);
+            }
+
+            return true;
+        }
+
+        #endregion
     }
 }
