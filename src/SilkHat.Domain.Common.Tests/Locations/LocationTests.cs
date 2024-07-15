e2e60405d03c951d4049075c2b98b1a77bcc72d5
Chris Russell <cgrussell@gmail.com>
2024-07-15 07:31:03 +0100

    Location with tests

---
+++ src/SilkHat.Domain.Common.Tests/Locations/LocationTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using SilkHat.Domain.Common.Locations;
+
+namespace SilkHat.Domain.Common.Tests.Locations
+{
+    public class LocationTests
+    {
+        [Theory]
+        [MemberData(nameof(A_OverlapsEntirely_B))]
+        [MemberData(nameof(A_OverlapsStartOf_B))]
+        [MemberData(nameof(A_OverlapsEndOf_B))]
+        public void Given_Locations_Overlap(Location a, Location b)
+        {
+            a.Overlaps(b).Should().BeTrue();
+        }
+        
+        [Theory]
+        [MemberData(nameof(A_OverlapsStartOf_B))]
+        public void Given_Locations_OverlapStart(Location a, Location b)
+        {
+            a.OverlapsStart(b).Should().BeTrue();
+        }
+        
+        [Theory]
+        [MemberData(nameof(A_OverlapsEndOf_B))]
+        public void Given_Locations_DoesNot_OverlapStart(Location a, Location b)
+        {
+            a.OverlapsStart(b).Should().BeFalse();
+        }
+        
+        [Theory]
+        [MemberData(nameof(A_OverlapsEndOf_B))]
+        public void Given_Locations_OverlapEnd(Location a, Location b)
+        {
+            a.OverlapsEnd(b).Should().BeTrue();
+        }
+        
+        [Theory]
+        [MemberData(nameof(A_OverlapsStartOf_B))]
+        public void Given_Locations_DoesNot_OverlapEnd(Location a, Location b)
+        {
+            a.OverlapsEnd(b).Should().BeFalse();
+        }
+
+        public static IEnumerable<object[]> A_OverlapsEndOf_B()
+        {
+            yield return new[]
+            {
+                new Location(new LocationPosition(10, 10), new LocationPosition(20, 20)),
+                new Location(new LocationPosition(0, 0), new LocationPosition(15, 15))
+            };
+        }
+
+        public static IEnumerable<object[]> A_OverlapsStartOf_B()
+        {
+            yield return new[]
+            {
+                new Location(new LocationPosition(0, 0), new LocationPosition(10, 10)),
+                new Location(new LocationPosition(1, 1), new LocationPosition(15, 15))
+            };
+        }
+
+        public static IEnumerable<object[]> A_OverlapsEntirely_B()
+        {
+            yield return new[]
+            {
+                new Location(new LocationPosition(0, 0), new LocationPosition(10, 10)),
+                new Location(new LocationPosition(1, 1), new LocationPosition(5, 5))
+            };
+        }
+    }
+}
---
+++ src/SilkHat.Domain.Common/Locations/Location.cs
@@ -0,0 +1,46 @@
+﻿namespace SilkHat.Domain.Common.Locations
+{
+    public record Location(LocationPosition Start, LocationPosition End)
+    {
+        public bool IsBefore(Location other)
+        {
+            return End.Character < other.Start.Character;
+        }
+
+        public bool IsAfter(Location other)
+        {
+            return Start.Character > other.End.Character;
+        }
+
+        public bool OverlapsStart(Location other)
+        {
+            return Start.Character < other.Start.Character &&
+                   End.Character > other.Start.Character;
+        }
+        
+        public bool OverlapsEnd(Location other)
+        {
+            return Start.Character < other.End.Character &&
+                   End.Character > other.End.Character;
+        }
+
+        public bool Overlaps(Location other)
+        {
+            return OverlapsStart(other) || OverlapsEnd(other);
+        }
+
+        public bool ContainedBy(Location other)
+        {
+            return Start.Character > other.Start.Character && 
+                   End.Character < other.End.Character;
+        }
+        
+        public bool Wraps(Location other)
+        {
+            return Start.Character < other.Start.Character && 
+                   End.Character > other.End.Character;
+        }
+    }
+
+    public record LocationPosition(int Character, int Line);
+}
